4bb22616fc350887f657f470894c6a5471047595
Nate Bowman <nlbowma2@illinois.edu>
2020-09-04 23:05:43 -0400

    Move actual workspace to subdirectory of ag_workspace
    Original plan of clearing out ag_workspace every time will not work
    because Docker creates subdirectories as root that would need to be
    deleted with sudo, which I do not want to do. The new organization also
    matches how PL already keeps local external grading jobs.

--- try_test.sh
+++ try_test.sh
@@ -2,10 +2,16 @@
 
 course_root=/home/nate/452/452-pl/
 regression_dir=`pwd`/regression_tests/
-workspace_dir=${regression_dir}/ag_workspace/
+base_workspace_dir=${regression_dir}/ag_workspace/
+base_results_dir=${regression_dir}/ag_test_results/
 submission_dir=${regression_dir}/submissions/
-results_dir=${regression_dir}/ag_test_results/
-job_dir=run1/  # TODO
+job_dir=run2/  # TODO
+
+workspace_dir=${base_workspace_dir}/${job_dir}
+results_dir=${base_results_dir}/${job_dir}
+
+mkdir ${workspace_dir}
+mkdir ${results_dir}
 
 cp -r tests ${workspace_dir}/tests
 cp -r ${submission_dir}/sample ${workspace_dir}/student
@@ -13,5 +19,5 @@ cp -r ${course_root}/serverFilesCourse ${workspace_dir}/serverFilesCourse
 
 docker run -it --rm \
     -v ${workspace_dir}:/grade \
-    -v ${results_dir}/${job_dir}:/grade/results \
+    -v ${results_dir}:/grade/results \
     eecarrier/c-and-python-v2 /grade/serverFilesCourse/python_autograder/run.sh
